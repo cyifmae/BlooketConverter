b68e9078e0a2ef95c44437c5212849b46203286c
cyifmae <cyifmae@gmail.com>
2026-03-24 09:51:27 -0600

    Update schoologyToBlooket.ts

--- src/handlers/schoologyToBlooket.ts
+++ src/handlers/schoologyToBlooket.ts
@@ -8,8 +8,10 @@ class schoologyToBlooketHandler implements FormatHandler {
   public ready: boolean = true;
 
   public supportedFormats: FileFormat[] = [
-    CommonFormats.SCHOOLOGY.supported("schoology", true, false),
-    CommonFormats.BLOOKET.supported("blooket", false, true)
+  CommonFormats.SCHOOLOGY.supported("schoology", true, false),
+  CommonFormats.BLOOKET.supported("blooket", false, true)
+  ];
+
   ];
   async init() {
     this.ready = true;
